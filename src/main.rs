92da1b862287b995e3aac4b11c358de71b10bb16
AridTag <AridTag@binarydepths.com>
2024-01-22 18:23:09 -0500

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                let mut stream = stream;
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
